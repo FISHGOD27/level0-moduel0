5e85f82f9e7d138a8936c6d13602b6c203b27c32
FISHGOD27 <106782906+FISHGOD27@users.noreply.github.com>
2022-06-08 11:58:27 -0700

    Update _b_pentagon_crazy.py

--- _02_code_flow/_b_pentagon_crazy.py
+++ _02_code_flow/_b_pentagon_crazy.py
@@ -25,34 +25,59 @@ if __name__ == '__main__':
 
 if __name__ == '__main__':
     window = turtle.Screen()
-    window.bgcolor('white')
+    window.bgcolor('black')
 
-    a=turtle.Turtle
+    a = turtle.Turtle()
     # Make the turtle shape 'turtle', .shape('turtle')
-    
+    a.shape('turtle')
     # Set the turtle speed to max (0)
-    
+    a.speed(0)
     # Set the turtle width to 1
-    
+    a.width(1)
     # Create a variable to hold the number of sides in a pentagon
-    
+    pentagon = 5
     # Create a variable to be the angle of 360 divided by the sides variable
-    
-    # Use a for loop to repeat ALL the following lines of code 360 times. 
-        
+    angle = 360/pentagon
+    # Use a for loop to repeat ALL the following lines of code 360 times.
+    a.penup()
+    a.goto(x=-270,y=0)
+    a.pendown()
+    for i in range(360):
+        if i==100:
+                a.width(2)
+        if i==200:
+                a.width(3)
+        a.pencolor(get_next_color(i))
+        a.forward(i)
+        a.right(angle+1)
+        a.hideturtle()
+    a.penup()
+    a.goto(x=270,y=0)
+    a.pendown()
+    for i in range(360):
+        if i==100:
+                a.width(2)
+        if i==200:
+                a.width(3)
+        a.pencolor(get_next_color(i))
+        a.forward(i)
+        a.right(angle+1)
+        a.hideturtle()
+
         # If the loop variable (i) is equal to 100, set the turtle width to 2
-        
+
         # If the loop variable (i) is equal to 200, set the turtle width to 3
-        
+
         # Use the get_next_color function to set the turtle pencolor,
+
         # *hint .pencolor(get_next_color(i))
-        
+
         # Move the turtle forward by the loop variable, *hint .forward(i)
-        
+
         # Turn the turtle to the right by the angle variable + 1
 
     # Hide your turtle so you can see the pattern.
-        
+
     # Check the pattern against the picture in the recipe. If it matches, you are done!
     
     # Variations:
