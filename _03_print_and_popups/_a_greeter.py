99b21a8ef102c481bb9c1130b8ce1bdb2718931d
Zachary <zach.dantsker@gmail.com>
2022-06-09 11:56:14 -0700

    I did some cool pop ups! yeah!

--- _03_print_and_popups/_a_greeter.py
+++ _03_print_and_popups/_a_greeter.py
@@ -6,16 +6,25 @@ if __name__ == '__main__':
     # Make a new window variable, window = Tk()
     window = Tk()
     # Hide the window using the window's .withdraw() method
-    window.withdraw()
+
     # Ask the user for their name and save it to a variable
     # name = simple dialog.askstring(title='Greeter', prompt="What is your name?")
-
+    name = simpledialog.askstring(title='Greeter', prompt="What is your name?")
     # Show a message box with your message using the .showinfo() method
-    
+    messagebox.showinfo(title='Greeter', message='hello '+name+'!')
+    messagebox.showinfo(title='Greeter', message='how has your day been?')
+    name = simpledialog.askstring(title='Greeter', prompt="how has your day been?")
+    messagebox.showinfo(title='Greeter', message='mine has also been '+name+'!')
     # Print your message to the console using the print() function
-    
+    print('good morning')
     # Show an error message using messagebox.showerror()
-
+    messagebox.showerror(title='Greeter', message='HeY! YOu arE reAlLy RaD!')
+    name = simpledialog.askstring(title='Greeter', prompt="YoU aRe ReALLY raD!?")
+    messagebox.showinfo(title='Greeter', message='I AgrEe!')
+    messagebox.showerror(title='Greeter', message='w0uLd yoU Like tO l1vE?')
+    name = simpledialog.askstring(title='Greeter', prompt="w0uLd yoU Like tO l1vE?")
+    messagebox.showerror(title='Greeter', message='mE t0!1!')
+    messagebox.showerror(title='Greeter', message='Go0byE!')
     # Run the window's .mainloop() method
 
     pass
--- _03_print_and_popups/_d_quiz_game.py
+++ _03_print_and_popups/_d_quiz_game.py
@@ -1,22 +1,24 @@
 from tkinter import messagebox, simpledialog, Tk
 
-# Create an if-main code block, *hint, type main then ctrl+space to auto-complete
-    
-    # Make a new window variable, window = Tk()
-    
-    # Hide the window using the window's .withdraw() method
-    
-    # 1. Create a variable to hold the user's score. Set it equal to zero. 
+    # Create an if-main code block, *hint, type main then ctrl+space to auto-complete
 
+    # Make a new window variable, window = Tk()
+window = Tk()
+    # Hide the window using the window's .withdraw() method
+window.withdraw()
+    # 1. Create a variable to hold the user's score. Set it equal to zero. 
+score = 0
     # ASK A QUESTION AND CHECK THE ANSWER
 
     #      // 2.  Ask the user a question 
-    
+question = simpledialog.askstring(title='Question', prompt="What is 5*5?")
     #      // 3.  Use an if statement to check if their answer is correct
-
-    #      // 4.  if the user's answer was correct, add one to their score 
- 
-    # MAKE MORE QUESTIONS. Ask more questions by repeating the above 
+if question=='25':
+    messagebox.showinfo(title='Good Job', message='Your not a total moron!')
+    #      // 4.  if the user's answer was correct, add one to their score
+else:
+    messagebox.showinfo(title='STUPID!', message='HOW ARE YOU SO DUMB! GO BACK TO KINDERGARTEN!!')
+    # MAKE MORE QUESTIONS. Ask more questions by repeating the above
     #      // Option: Subtract a point from their score for a wrong answer
  
     # After all the questions have been asked, tell the user their final score
